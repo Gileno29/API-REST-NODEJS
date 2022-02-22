a4036e2f270ab5f0d40f5b8b3cf3d6672c3df08c
gileno <gileno.cr.duarte@gmail.com>
2022-02-22 17:40:39 -0300

    problemas com a conexao no banco

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const custonExpress = require('./config/custonExpress')
 const app = custonExpress()
+const conexao= require('./infraestrutura/conexao')
+
+
+
+conexao.connect(erro=>{
+        if(erro){
+            console.log(erro)
+        }else{
+            console.log('conectado')
+        }
+    })
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
 //req --> o que foi enviado para a  gente
--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,12 @@
-const mysql = require('msyql')
+const mysql = require('mysql')
 
 const conexao = mysql.createConnection({
     host:'localhost',
-    user:'root',
-    password:'',
+    user:'teste',
+    password:'1234',
     port:3306,
     database:'agendapetshop'
 })
 
+module.exports=conexao
 
