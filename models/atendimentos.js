fd305e15895f1d355bbaae242e0de61830849cc8
gileno <gileno.cr.duarte@gmail.com>
2022-02-26 13:00:02 -0300

    iniciando validações

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,19 +4,40 @@ const req = require('express/lib/request')
 //const res = require('express/lib/response')
 class Atendimentos {
     adiciona(atendimento, res, req){
+        
+
         const sql = 'INSERT INTO Atendimentos SET ?'
         const dataCriacao= moment().format('YYYY-MM-DD  HH:MM:SS')
         const data= moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD  HH:MM:SS')
-        const atendimentoDatado= {...atendimento, dataCriacao, data}
-        conexao.query(sql,atendimentoDatado,(err, result)=>{
-            if(err){
-                res.status(400).json(err)
-            }else{
-                res.status(201).json(result)
-            }
+        const dataEhValida= moment(data).isSameOrAfter()
+        const clienteEhValido= atendimento.cliente.length()>= 5
+        const validacao=[
+            
+            { }
+        
+        ]
+
+
+        if (!dataEhValida){
+            res.send('data é invalida, não é possivel cadastrar uma para um periodo anterior ao atual')
+
+        }else if(!clienteEhValido){
+            res.send('cliente é invalido, o nome é muito curto')
+
+        }else{
+            const atendimentoDatado= {...atendimento, dataCriacao, data}
+            conexao.query(sql,atendimentoDatado,(err, result)=>{
+                if(err){
+                    res.status(400).json(err)
+                }else{
+                    res.status(201).json(result)
+                }
             
 
-        })
+            })
+
+        }
+        
 
 
     }
